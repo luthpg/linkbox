bd9bbdf8675525c58e6a5b9813800b542201c962
luth <88308177+luthpg@users.noreply.github.com>
2025-07-02 22:19:59 +0900

    環境変数のバリデーションを更新し、いくつかのフィールドをオプショナルに変更しました。これにより、未使用の環境変数が適切に処理されるようになります。

--- env.ts
+++ env.ts
@@ -3,14 +3,14 @@ import { z } from 'zod';
 
 export const env = createEnv({
   server: {
-    CONVEX_DEPLOYMENT: z.string().min(1),
-    CLERK_SECRET_KEY: z.string().min(1),
-    CLERK_WEBHOOK_SECRET: z.string().min(1),
+    CONVEX_DEPLOYMENT: z.string().min(1).optional(), // not for used
+    CLERK_SECRET_KEY: z.string().min(1).optional(), // not for used
+    CLERK_WEBHOOK_SECRET: z.string().min(1).optional(), // for convex http action
   },
   client: {
     NEXT_PUBLIC_CONVEX_URL: z.string().url().min(1),
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1),
-    NEXT_PUBLIC_CLERK_FRONTEND_API_URL: z.string().url().min(1),
+    NEXT_PUBLIC_CLERK_FRONTEND_API_URL: z.string().url().min(1).optional(), // for convex http action
   },
   experimental__runtimeEnv: {
     NEXT_PUBLIC_CONVEX_URL: process.env.NEXT_PUBLIC_CONVEX_URL,
