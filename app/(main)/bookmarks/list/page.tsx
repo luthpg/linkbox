b486dfcdec1fcdf9d1c461132b567707fb1de2d6
luth <88308177+luthpg@users.noreply.github.com>
2025-07-05 00:06:44 +0900

    ブックマークリストコンポーネントからAPIクエリを削除し、`bookmarks`プロパティを直接渡さないように変更しました。これにより、コンポーネントのシンプルさが向上しました。

--- app/(main)/bookmarks/list/page.tsx
+++ app/(main)/bookmarks/list/page.tsx
@@ -1,15 +1,11 @@
 'use client';
 
 import { BookmarkListClient } from '@/components/custom/BookmarkListClient';
-import { api } from '@/convex/_generated/api';
-import { useQuery } from 'convex/react';
 
 export default function BookmarksPage() {
-  const bookmarks = useQuery(api.bookmarks.getBookmarks);
   return (
     <div className="p-3">
       <BookmarkListClient
-        bookmarks={bookmarks == null ? undefined : bookmarks}
         tag={null}
       />
     </div>
